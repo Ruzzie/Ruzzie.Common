b36be5c1657b541768c26634071c292126e2e0cc
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2015-12-14 07:33:54 +0100

    Moved code to common assembly for a bit more coarse grained modularity

--- src/Ruzzie.Common/Ruzzie.Common.Portable.UnitTests/Properties/AssemblyInfo.cs
+++ src/Ruzzie.Common/Ruzzie.Common.Portable.UnitTests/Properties/AssemblyInfo.cs
@@ -1,18 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ruzzie.Common.Numerics")]
+[assembly: AssemblyTitle("Ruzzie.Common.Portable.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ruzzie.Common.Numerics")]
-[assembly: AssemblyCopyright("Copyright © Dorus Verhoeckx 2015")]
+[assembly: AssemblyProduct("Ruzzie.Common.Portable.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad870e12-9851-41fe-b13f-6d28f28e4d7b")]
 
 // Version information for an assembly consists of the following four values:
 //
--- src/Ruzzie.Common/Ruzzie.Common/AssemblyInfo.cs
+++ src/Ruzzie.Common/Ruzzie.Common/AssemblyInfo.cs
@@ -1,26 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Resources;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ruzzie.Common.Numerics.UnitTests")]
+[assembly: AssemblyTitle("Ruzzie.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ruzzie.Common.Numerics.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Ruzzie.Common")]
+[assembly: AssemblyCopyright("Copyright © Dorus Verhoeckx 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cd4b52c3-f674-4207-80d1-be5a4134dc40")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
