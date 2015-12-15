9b41f90c0d4ce0c8509eeb8d2d266a1c85e4d07c
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2015-12-15 05:51:05 +0100

    added smoke tests for Volatile class

---
+++ src/Ruzzie.Common/Ruzzie.Common.Shared.UnitTests/Threading/VolatileTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Ruzzie.Caching.Threading;
+
+namespace Ruzzie.Common.Shared.UnitTests.Threading
+{
+    [TestFixture]
+    public class VolatileTests
+    {
+        [Test]
+        public void ReadApiSmokeTest()
+        {
+            //Arrange
+            ValueReferenceType value = new ValueReferenceType();
+            //Act
+            var read = Volatile.Read(ref value);
+            //Act
+            Assert.That(value, Is.EqualTo(read));
+        }
+
+        [Test]
+        public void WriteApiSmokeTest()
+        {
+            //Arrange
+            ValueReferenceType value = new ValueReferenceType();
+            ValueReferenceType valueToWrite = new ValueReferenceType();
+            //Act
+            Volatile.Write(ref value,valueToWrite);
+            //Assert
+            Assert.That(value, Is.EqualTo(valueToWrite));
+        }
+
+        class ValueReferenceType
+        {
+        }
+    }
+}
