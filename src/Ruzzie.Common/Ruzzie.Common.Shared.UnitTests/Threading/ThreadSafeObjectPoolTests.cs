fc4431ea634b5b3626047e4a5a4584cde08a6456
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2016-12-22 09:38:27 +0100

    - added ThreadSafeObjectPool class to library

---
+++ src/Ruzzie.Common/Ruzzie.Common.Shared.UnitTests/Threading/ThreadSafeObjectPoolTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+#if ! PORTABLE
+using System;
+using Moq;
+#endif
+using NUnit.Framework;
+using Ruzzie.Common.Threading;
+
+namespace Ruzzie.Common.Shared.UnitTests.Threading
+{
+    [TestFixture]
+    public class ThreadSafeObjectPoolTests
+    {
+        [Test]
+        public void SmokeTest()
+        {
+            ThreadSafeObjectPool<SHA1> sha1ObjectPool = new ThreadSafeObjectPool<SHA1>(SHA1.Create, 8);
+
+            Parallel.For(0, 100000, i => { sha1ObjectPool.ExecuteOnAvailableObject(sha1 => sha1.ComputeHash(Encoding.Unicode.GetBytes("ThreadSafeObjectPoolTests SmokeTest" + i))); });
+        }
+
+#if ! PORTABLE
+        [Test]
+        public void DisposeTest()
+        {
+            Mock<IDisposable> disposableMock = new Mock<IDisposable>();
+            var sha1ObjectPool = new ThreadSafeObjectPool<IDisposable>(() => disposableMock.Object, 1);
+
+            //Act
+            sha1ObjectPool.Dispose();
+
+            //Assert
+            disposableMock.Verify(disposable => disposable.Dispose(), Times.Once);
+        }
+#endif
+        [Test]
+        public void ContentionTest()
+        {
+            int objectCount = 1;
+            ThreadSafeObjectPool<ContentionObject> pool = new ThreadSafeObjectPool<ContentionObject>(() => new ContentionObject((objectCount++).ToString()), 2);
+
+            //Each execution should be handled by a different object
+            var resultOne = Task.Run(() => pool.ExecuteOnAvailableObject(o => o.ExecuteAndHoldLock(100)));
+            var resultTwo = Task.Run(() => pool.ExecuteOnAvailableObject(o => o.ExecuteAndHoldLock(100)));
+
+            Assert.That(resultOne.Result, Is.Not.EqualTo(resultTwo.Result));
+        }
+
+        class ContentionObject
+        {
+            private string Name { get; }
+
+            public ContentionObject(string name)
+            {
+                Name = name;
+            }
+
+            public string ExecuteAndHoldLock(int sleepTimeInMillis)
+            {
+                Thread.Sleep(sleepTimeInMillis);
+                return Name;
+            }
+        }
+    }
+}
---
+++ src/Ruzzie.Common/Ruzzie.Common/Threading/ThreadSafeObjectPool.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+
+namespace Ruzzie.Common.Threading
+{
+    /// <summary>
+    /// Contains a pool of objects that can be used to execute a method thread safe. The pool is created so multiple threads can execute simulanuously, this is done by creating multiple instances of type T.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="IDisposable" />
+    public class ThreadSafeObjectPool<T> : IDisposable
+    {
+        private readonly int _poolSize;
+        private readonly int _indexMask;
+        private readonly T[] _objects;
+        private readonly object[] _lockSlots;
+        private VolatileLong _index = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThreadSafeObjectPool{T}"/> class.
+        /// </summary>
+        /// <param name="createTypeFactory">The factory method to create instances of type T</param>
+        /// <param name="poolSize">Size of the pool.</param>
+        /// <exception cref="ArgumentNullException">When the createTypeFactory is null. </exception>
+        /// <exception cref="ArgumentOutOfRangeException">When the poolsize is less than or equal to 0.</exception>
+        public ThreadSafeObjectPool(Func<T> createTypeFactory, int poolSize = 16)
+        {
+            if (createTypeFactory == null)
+            {
+                throw new ArgumentNullException(nameof(createTypeFactory));
+            }
+
+            if (poolSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(poolSize));
+            }
+
+            _poolSize = Numerics.PowerOfTwoHelper.FindNearestPowerOfTwoEqualOrGreaterThan(poolSize);
+
+            _lockSlots = new object[_poolSize];
+            _objects = new T[_poolSize];
+
+            for (int i = 0; i < _poolSize; i++)
+            {
+                _lockSlots[i] = new object();
+                _objects[i] = createTypeFactory();
+            }
+            _indexMask = _poolSize - 1;
+        }
+
+        /// <summary>
+        /// Executes the method the on available object. This is thread safe.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="funcToExecuteInPool">The function to execute in pool.</param>
+        /// <returns>The result from the executed method.</returns>
+        public TResult ExecuteOnAvailableObject<TResult>(Func<T, TResult> funcToExecuteInPool)
+        {
+            if (funcToExecuteInPool == null)
+            {
+                throw new ArgumentNullException(nameof(funcToExecuteInPool));
+            }
+
+            long currentIndex = _index.CompilerFencedValue;
+            do
+            {
+                currentIndex = currentIndex & _indexMask;
+
+                if (Monitor.TryEnter(_lockSlots[currentIndex]))
+                {
+                    try
+                    {
+                        return funcToExecuteInPool.Invoke(_objects[currentIndex]);
+                    }
+                    finally
+                    {
+                        Monitor.Exit(_lockSlots[currentIndex]);
+                        _index.AtomicIncrement();
+                    }
+                }
+                ++currentIndex;
+            } while (true);
+        }
+
+
+        /// <summary>
+        /// Disposed unmanaged resources. If T is of IDisposable it will dispose all objects in the object pool.
+        /// </summary>
+        /// <param name="disposing"></param>
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (typeof(T).IsAssignableFrom(typeof(IDisposable)))
+                {
+                    // Dispose of resources held by this instance.
+                    for (int i = 0; i < _poolSize; i++)
+                    {
+                        var currentObject = _objects[i] as IDisposable;
+                        currentObject?.Dispose();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposed unmanaged resources. If T is of IDisposable it will dispose all objects in the object pool
+        /// </summary>
+        public void Dispose()
+        {
+            // Dispose of resources held by this instance.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
