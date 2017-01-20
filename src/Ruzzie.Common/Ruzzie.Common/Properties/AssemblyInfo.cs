7e3b3716264e315738d290a44a725f751485c7b1
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2017-01-20 09:06:41 +0100

    - added new assemblyinfo

--- src/Ruzzie.Common/Ruzzie.Common/Properties/AssemblyInfo.cs
+++ src/Ruzzie.Common/Ruzzie.Common/Properties/AssemblyInfo.cs
@@ -1,20 +1,36 @@
-﻿//using System.Reflection;
-//using System.Runtime.InteropServices;
+﻿using System;
+using System.Resources;
+using System.Reflection;
+#if !PORTABLE
+using System.Runtime.InteropServices;
+#endif
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ruzzie.Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("D Crafting")]
+[assembly: AssemblyProduct("Ruzzie.Common")]
+[assembly: AssemblyCopyright("Copyright © Dorus Verhoeckx 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
 
-//// General Information about an assembly is controlled through the following
-//// set of attributes. Change these attribute values to modify the information
-//// associated with an assembly.
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("")]
-//[assembly: AssemblyProduct("Ruzzie.Common")]
-//[assembly: AssemblyTrademark("")]
-
-//#if !PORTABLE
-//// Setting ComVisible to false makes the types in this assembly not visible
-//// to COM components.  If you need to access a type in this assembly from
-//// COM, set the ComVisible attribute to true on that type.
-//[assembly: ComVisible(false)]
-//#endif
-//// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("dd19fd5d-893c-49f6-bc0c-8c62824fd801")]
+#if !PORTABLE
+[assembly: ComVisible(false)]
+#endif
