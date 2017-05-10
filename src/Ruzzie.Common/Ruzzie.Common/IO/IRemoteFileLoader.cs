72715dbea2ab0f63c380ae770a5c4380a00ec427
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2017-05-10 08:03:13 +0200

    - moved remote file loader from ruzzie.mtg.core to ruzzie.common.io

---
+++ src/Ruzzie.Common/Ruzzie.Common/IO/IRemoteFileLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Ruzzie.Common.IO
+{
+#if !PORTABLE
+    /// <summary>
+    /// Interface for a remote file loader.
+    /// </summary>
+    public interface IRemoteFileLoader
+    {
+        /// <summary>
+        /// Returns the local (cached) file, and downloads the remote file first if it is newer.
+        /// </summary>
+        /// <returns>The FileInfo to the file.</returns>
+        FileInfo GetLocalOrDownloadIfNewer();
+    }
+#endif
+}
---
+++ src/Ruzzie.Common/Ruzzie.Common/IO/IRemoteFileMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ruzzie.Common.IO
+{
+    /// <summary>
+    /// Metadata for a remote file.
+    /// </summary>
+    public interface IRemoteFileMetaData
+    {
+        /// <summary>
+        /// Gets or sets the last modified time UTC.
+        /// </summary>
+        /// <value>
+        /// The last modified time UTC.
+        /// </value>
+        DateTime? LastModifiedTimeUtc { get; set; }
+    }
+}
